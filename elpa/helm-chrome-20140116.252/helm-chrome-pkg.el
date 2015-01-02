ea1560fa94e311ed5a4d2a00080d8e1294260911
zeltak <zeltak@gmail.com>
2015-01-02 09:39:11 +0200

    zuni Fri Jan  2 09:39:11 IST 2015

---
+++ elpa/helm-chrome-20140116.252/helm-chrome-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-chrome" "20140116.252" "Helm interface for Chrome bookmarks" '((helm "1.0") (cl-lib "0.3") (emacs "24")) :url "https://github.com/kawabata/helm-chrome" :keywords '("tools"))
