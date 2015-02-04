32e62cb340d6b967bd7d065441aa630ebb584586
zeltak <zeltak@gmail.com>
2015-02-04 17:19:04 +0200

    zuni Wed Feb  4 17:19:04 IST 2015

---
+++ el-get/.status.el
@@ -0,0 +1,28 @@
+((cl-lib status "installed" recipe
+	 (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+		("el-get.*\\.el$" "methods/")
+		:load "el-get.el" :post-init
+		(when
+		    (memq 'el-get
+			  (bound-and-true-p package-activated-list))
+		  (message "Deleting melpa bootstrap el-get")
+		  (unless package--initialized
+		    (package-initialize t))
+		  (when
+		      (package-installed-p 'el-get)
+		    (let
+			((feats
+			  (delete-dups
+			   (el-get-package-features
+			    (el-get-elpa-package-directory 'el-get)))))
+		      (el-get-elpa-delete-package 'el-get)
+		      (dolist
+			  (feat feats)
+			(unload-feature feat t))))
+		  (require 'el-get))))
+ (hydra status "installed" recipe
+	(:name hydra :description "make Emacs bindings that stick around" :type github :depends
+	       (cl-lib)
+	       :pkgname "abo-abo/hydra")))
